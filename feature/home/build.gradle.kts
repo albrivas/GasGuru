8952c5a046304d68e2a1b53ce799155b12309a28
Alberto Rivas <albrivas95@outlook.com>
2023-02-28 20:56:05 +0100

    feat: add uikit module

--- feature/home/build.gradle.kts
+++ feature/home/build.gradle.kts
@@ -44,6 +44,7 @@ dependencies {
     implementation(project(":core:ui"))
     implementation(project(":core:domain"))
     implementation(project(":core:model"))
+    implementation(project(":core:uikit"))
     androidTestImplementation(project(":core:testing"))
 
     // Core Android dependencies
