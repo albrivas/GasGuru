21a9c7f72bdfcb45de48a61c439cfa951d9196b5
Alberto Rivas <38986603+albrivas@users.noreply.github.com>
2024-01-21 18:46:35 +0100

    Feature/12 onboarding select fuel (#18)
    * feat: rename module onboarding-welcome to onboarding
    
    * fix: change the R implementation
    
    * chore: add strings
    
    * build: update kotlin, gradle and compose version
    
    * feat: add screen "Onboarding Fuel Preferences" with all logic
    
    * feat: add app theme to preview welcome onboarding
    
    * refactor: rename package onboarding from java to kotlin
    
    * feat: add new module commons
    
    * feat: add strings
    
    * feat: add logic to selected fuel and send selection to viewmodel
    
    * feat: ui layer has visibility of model layer

---
+++ feature/onboarding/src/main/kotlin/com/albrivas/fuelpump/feature/onboarding_welcome/ui/OnboardingWelcomeScreen.kt
@@ -0,0 +1,102 @@
+package com.albrivas.fuelpump.feature.onboarding_welcome.ui
+
+import android.Manifest.permission.ACCESS_FINE_LOCATION
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.albrivas.fuelpump.core.uikit.components.FuelPumpButton
+import com.albrivas.fuelpump.core.uikit.theme.GrayLight
+import com.albrivas.fuelpump.core.uikit.theme.MyApplicationTheme
+import com.albrivas.fuelpump.feature.onboarding.R
+import com.albrivas.fuelpump.core.uikit.R as RUikit
+
+
+@Composable
+fun OnboardingWelcomeScreenRoute(navigateToSelectFuel: () -> Unit) {
+    OnboardingWelcomeScreen(navigateToSelectFuel)
+}
+
+@Composable
+internal fun OnboardingWelcomeScreen(navigateToSelectFuel: () -> Unit = {}) {
+
+    var locationPermissionGranted by remember { mutableStateOf(false) }
+
+    val requestPermissionLauncher =
+        rememberLauncherForActivityResult(
+            contract = ActivityResultContracts.RequestPermission(),
+            onResult = { isGranted: Boolean -> locationPermissionGranted = isGranted })
+
+
+    LaunchedEffect(Unit) { requestPermissionLauncher.launch(ACCESS_FINE_LOCATION) }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(start = 16.dp, top = 24.dp, end = 16.dp, bottom = 24.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Image(
+            painter = painterResource(id = RUikit.drawable.ic_welcome),
+            contentDescription = "Welcome image",
+            modifier = Modifier.padding(top = 30.dp)
+        )
+
+        Spacer(modifier = Modifier.height(66.dp))
+        Text(
+            text = stringResource(id = R.string.welcome),
+            style = MaterialTheme.typography.titleLarge
+        )
+
+        Spacer(modifier = Modifier.height(28.dp))
+        Text(
+            text = stringResource(id = R.string.welcome_text),
+            color = GrayLight,
+            textAlign = TextAlign.Center,
+            style = MaterialTheme.typography.displayMedium
+        )
+
+        Spacer(modifier = Modifier.height(24.dp))
+        Text(
+            text = stringResource(id = R.string.welcome_permission),
+            color = GrayLight,
+            textAlign = TextAlign.Center,
+            style = MaterialTheme.typography.displayMedium
+        )
+
+        Spacer(modifier = Modifier.weight(1f))
+        FuelPumpButton(
+            onClick = navigateToSelectFuel,
+            enabled = locationPermissionGranted,
+            text = R.string.welcome_button,
+            modifier = Modifier.padding(bottom = 17.dp)
+        )
+    }
+}
+
+@Composable
+@Preview
+private fun OnboardingWelcomeScreenPreview() {
+    MyApplicationTheme {
+        OnboardingWelcomeScreen()
+    }
+}
