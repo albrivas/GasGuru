740126d45d4cc32b26e7472f4714e633e042e769
Alberto Rivas <albrivas95@outlook.com>
2024-01-31 21:18:36 +0100

    feat: get fuel stations by location in ui layer

---
+++ feature/home/src/main/java/com/albrivas/fuelpump/feature/home/ui/HomeUiState.kt
@@ -0,0 +1,9 @@
+package com.albrivas.fuelpump.feature.home.ui
+
+import com.albrivas.fuelpump.core.model.data.FuelStation
+
+sealed interface HomeUiState {
+    data object Loading : HomeUiState
+    data object Error: HomeUiState
+    data class Success(val fuelStations: List<FuelStation>): HomeUiState
+}
