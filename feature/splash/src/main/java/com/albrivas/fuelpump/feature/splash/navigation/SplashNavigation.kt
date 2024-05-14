be3640f3daee4f423ba6e918803efb1fbc864a81
Alberto Rivas <alberto.rivas@the-cocktail.com>
2024-05-14 23:07:07 +0200

    feat: implement new navigation compose in all screens

--- feature/splash/src/main/java/com/albrivas/fuelpump/feature/splash/navigation/SplashNavigation.kt
+++ feature/splash/src/main/java/com/albrivas/fuelpump/feature/splash/navigation/SplashNavigation.kt
@@ -3,13 +3,17 @@ package com.albrivas.fuelpump.feature.splash.navigation
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import com.albrivas.fuelpump.feature.splash.ui.SplashScreenRoute
+import kotlinx.serialization.Serializable
 
 const val splashRoute = "splash_route"
 fun NavGraphBuilder.splashScreen(navigateToOnboarding: () -> Unit, navigateToHome: () -> Unit) {
-    composable(route = splashRoute) {
+    composable<SplashRoute> {
         SplashScreenRoute(
             navigateToOnboarding = navigateToOnboarding,
             navigateToHome = navigateToHome
         )
     }
 }
+
+@Serializable
+data object SplashRoute
