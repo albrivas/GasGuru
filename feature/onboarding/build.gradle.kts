16ad1cf482846ae671b4668d0f71f444d84f95d5
Alberto Rivas <alberto.rivas@the-cocktail.com>
2024-05-14 23:06:30 +0200

    build: update navigation compose

--- feature/onboarding/build.gradle.kts
+++ feature/onboarding/build.gradle.kts
@@ -3,6 +3,8 @@ plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.kapt)
+    alias(libs.plugins.kotlin.serialization)
+
 }
 
 android {
@@ -67,6 +69,9 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.permission)
+    implementation(libs.androidx.navigation.compose)
+
+    implementation(libs.kotlinx.serialization.json)
     // Tooling
     debugImplementation(libs.androidx.compose.ui.tooling)
     // Instrumented tests
