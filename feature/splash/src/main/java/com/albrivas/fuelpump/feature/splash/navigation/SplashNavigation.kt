18080caeedcdfdbf6b85ee6a7dbbcfd763ac00f5
Alberto Rivas <alberto.rivas@the-cocktail.com>
2024-05-14 23:12:23 +0200

    refactor: remove unused const

--- feature/splash/src/main/java/com/albrivas/fuelpump/feature/splash/navigation/SplashNavigation.kt
+++ feature/splash/src/main/java/com/albrivas/fuelpump/feature/splash/navigation/SplashNavigation.kt
@@ -5,7 +5,6 @@ import androidx.navigation.compose.composable
 import com.albrivas.fuelpump.feature.splash.ui.SplashScreenRoute
 import kotlinx.serialization.Serializable
 
-const val splashRoute = "splash_route"
 fun NavGraphBuilder.splashScreen(navigateToOnboarding: () -> Unit, navigateToHome: () -> Unit) {
     composable<SplashRoute> {
         SplashScreenRoute(
