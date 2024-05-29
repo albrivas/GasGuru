bcb0de57d76d12c7e1fe43d101298a6cfbaa2de5
Alberto Rivas <38986603+albrivas@users.noreply.github.com>
2024-05-29 16:09:16 +0200

    Feature/30 crashlytics (#32)
    * feat: add google-services.json
    
    * refactor: remove malformed copyright
    
    * feat: add crashlytics
    
    ---------
    
    Co-authored-by: Alberto Rivas <alberto.rivas@the-cocktail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,3 @@
-import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
-
-@Suppress("DSL_SCOPE_VIOLATION")
 plugins {
-    alias(libs.plugins.com.github.ben.manes.versions)
-    alias(libs.plugins.nl.littlerobots.version.catalog.update)
     alias(libs.plugins.kotlin.serialization) apply false
-}
-
-fun isNonStable(version: String): Boolean {
-    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.toUpperCase().contains(it) }
-    val regex = "^[0-9,.v-]+(-r)?$".toRegex()
-    val isStable = stableKeyword || regex.matches(version)
-    return isStable.not()
-}
-
-tasks.withType<DependencyUpdatesTask> {
-    rejectVersionIf {
-        isNonStable(candidate.version) && !isNonStable(currentVersion)
-    }
 }
