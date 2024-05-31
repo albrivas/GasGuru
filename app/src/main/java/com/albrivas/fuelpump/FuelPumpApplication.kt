84392eb426c58ec78ff5baf1c4b2b43d32f1f2c4
Alberto Rivas <albrivas95@outlook.com>
2024-05-31 20:20:58 +0200

    feat: apply detekt rules

--- app/src/main/java/com/albrivas/fuelpump/FuelPumpApplication.kt
+++ app/src/main/java/com/albrivas/fuelpump/FuelPumpApplication.kt
@@ -1,10 +1,7 @@
 package com.albrivas.fuelpump
 
 import android.app.Application
-import com.albrivas.fuelpump.core.data.repository.OfflineFuelStationRepository
 import dagger.hilt.android.HiltAndroidApp
-import kotlinx.coroutines.coroutineScope
-import javax.inject.Inject
 
 @HiltAndroidApp
 class FuelPumpApplication : Application()
