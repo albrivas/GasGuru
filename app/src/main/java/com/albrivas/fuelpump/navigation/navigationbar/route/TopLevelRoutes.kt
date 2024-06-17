06857d517fad6cfb4a8e67a6152d9f91a7f90148
Alberto Rivas <38986603+albrivas@users.noreply.github.com>
2024-06-17 23:04:26 +0200

    fix: colour of the selected option in the navigation bar (#69)

--- app/src/main/java/com/albrivas/fuelpump/navigation/navigationbar/route/TopLevelRoutes.kt
+++ app/src/main/java/com/albrivas/fuelpump/navigation/navigationbar/route/TopLevelRoutes.kt
@@ -1,6 +1,8 @@
 package com.albrivas.fuelpump.navigation.navigationbar.route
 
+import com.albrivas.feature.station_map.navigation.StationMapRoute
 import com.albrivas.fuelpump.core.uikit.R
+import com.albrivas.fuelpump.feature.fuel_list_station.navigation.FuelStationListRoute
 import kotlinx.serialization.Serializable
 
 @Serializable
@@ -22,4 +24,15 @@ sealed class TopLevelRoutes {
         val icon: Int = R.drawable.ic_profile,
         val route: String = "profile_route"
     ) : TopLevelRoutes()
+
+    companion object {
+        fun fromRoute(route: String?): String? {
+            return when  {
+                route?.contains("${StationMapRoute::class.simpleName}") == true -> Map().route
+                route?.contains("${FuelStationListRoute::class.simpleName}") == true -> List().route
+                route?.contains("ProfileRoute") == true -> Profile().route
+                else -> null
+            }
+        }
+    }
 }
