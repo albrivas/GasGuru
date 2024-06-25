b92f01b2625c8aa41e6c97cefc0ba540305ceb88
Alberto Rivas <albrivas95@outlook.com>
2024-06-25 12:19:56 +0200

    feat: get station by id

--- core/database/src/main/java/com/albrivas/fuelpump/core/database/dao/FuelStationDao.kt
+++ core/database/src/main/java/com/albrivas/fuelpump/core/database/dao/FuelStationDao.kt
@@ -1,14 +1,3 @@
-/*
- * File: FuelStationDao.kt
- * Project: FuelPump
- * Module: FuelPump.core.database.main
- * Last modified: 1/5/23, 12:05 AM
- *
- * Created by albertorivas on 1/5/23, 12:13 AM
- * Copyright © 2023 Alberto Rivas. All rights reserved.
- *
- */
-
 package com.albrivas.fuelpump.core.database.dao
 
 import androidx.room.Dao
@@ -29,4 +18,7 @@ interface FuelStationDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertFuelStation(items: List<FuelStationEntity>)
+
+    @Query("SELECT * FROM `fuel-station` WHERE idServiceStation = :id")
+    fun getFuelStationById(id: Int): Flow<FuelStationEntity>
 }
