49c428f01646389eb50181d331c8d608064336d5
Alberto Rivas <38986603+albrivas@users.noreply.github.com>
2024-07-18 15:31:17 +0200

    Feature - Welcome screen UI test (#89)
    * build: add junit5
    
    * feat: add ui test for welcome screen
    
    * chore: remove unused test and move from unitTest to test package
    
    * feat: check ui test when user make a push
    
    * feat: change androidTestImplementation to testImplementation
    
    * Update main.yml
    
    * Update main.yml
    
    * Update main.yml
    
    * feat: create a base test
    
    * feat: remove unnecessary sourceSets
    
    * feat: update api-level in github workflow
    
    * Update main.yml
    
    * Update main.yml
    
    * Update main.yml

--- core/database/src/test/java/com/albrivas/fuelpump/core/database/dao/FuelStationDaoTest.kt
+++ core/database/src/test/java/com/albrivas/fuelpump/core/database/dao/FuelStationDaoTest.kt
@@ -1,32 +1,12 @@
-/*
- * File: FuelStationDaoTest.kt
- * Project: FuelPump
- * Module: FuelPump.core.database.androidTest
- * Last modified: 1/7/23, 4:31 PM
- *
- * Created by albertorivas on 1/7/23, 4:31 PM
- * Copyright © 2023 Alberto Rivas. All rights reserved.
- *
- */
-
 package com.albrivas.fuelpump.core.database.dao
 
 import android.content.Context
 import androidx.room.Room
-import androidx.test.core.app.ApplicationProvider
 import app.cash.turbine.test
 import com.albrivas.fuelpump.core.database.FuelPumpDatabase
 import com.albrivas.fuelpump.core.database.model.FuelStationEntity
 import com.albrivas.fuelpump.core.model.data.FuelType
-import com.albrivas.fuelpump.core.testing.CoroutinesTestRule
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.test.runTest
-import org.junit.After
-import org.junit.Assert.assertEquals
-import org.junit.Before
-import org.junit.Rule
-import org.junit.Test
-
 
 @ExperimentalCoroutinesApi
 class FuelStationDaoTest {
@@ -34,12 +14,12 @@ class FuelStationDaoTest {
     private lateinit var fuelStationDao: FuelStationDao
     private lateinit var db: FuelPumpDatabase
 
-    @get: Rule
-    val dispatcherRule = CoroutinesTestRule()
+    @get: org.junit.Rule
+    val dispatcherRule = com.albrivas.fuelpump.core.testing.CoroutinesTestRule()
 
-    @Before
+    @org.junit.Before
     fun createDb() {
-        val context = ApplicationProvider.getApplicationContext<Context>()
+        val context = androidx.test.core.app.ApplicationProvider.getApplicationContext<Context>()
         db = Room.inMemoryDatabaseBuilder(
             context,
             FuelPumpDatabase::class.java
@@ -47,19 +27,19 @@ class FuelStationDaoTest {
         fuelStationDao = db.fuelStationDao()
     }
 
-    @After
+    @org.junit.After
     fun closeDb() {
         db.close()
     }
 
-    @Test
-    fun fuelStationDao_insert_items() = runTest {
+    @org.junit.Test
+    fun fuelStationDao_insert_items() = kotlinx.coroutines.test.runTest {
         fuelStationDao.insertFuelStation(listFuelStation)
 
         val result = fuelStationDao.getFuelStations(FuelType.GASOLINE_95.name)
 
         result.test {
-            assertEquals(awaitItem(), listFuelStation)
+            org.junit.Assert.assertEquals(awaitItem(), listFuelStation)
             cancel()
         }
     }
