982aac1bb34de43dcba6799538c3574b5f981cb1
Alberto Rivas <albrivas95@outlook.com>
2024-07-18 23:02:53 +0200

    fix: add RegisterExtension annotation

--- core/testing/src/main/java/com/albrivas/fuelpump/core/testing/BaseTest.kt
+++ core/testing/src/main/java/com/albrivas/fuelpump/core/testing/BaseTest.kt
@@ -1,12 +1,16 @@
 package com.albrivas.fuelpump.core.testing
 
 import android.content.Context
+import androidx.compose.ui.test.ExperimentalTestApi
 import androidx.test.core.app.ApplicationProvider
 import de.mannodermaus.junit5.compose.createComposeExtension
+import org.junit.jupiter.api.extension.RegisterExtension
 
 open class BaseTest {
 
-    @OptIn(androidx.compose.ui.test.ExperimentalTestApi::class)
+    @OptIn(ExperimentalTestApi::class)
+    @Suppress("JUnitMalformedDeclaration")
+    @RegisterExtension
     @JvmField
     val extension = createComposeExtension()
 
