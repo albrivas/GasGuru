c21de6386025fae13389b9b0c6279b6873af4b57
Alberto Rivas <albrivas95@outlook.com>
2024-07-28 17:26:04 +0200

    feat: create base module profile

---
+++ feature/profile/src/main/java/com/albrivas/fuelpump/profile/ProfileScreen.kt
@@ -0,0 +1,20 @@
+package com.albrivas.fuelpump.profile
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+internal fun ProfileScreenRoute() {
+    return ProfileScreen()
+}
+
+@Composable
+internal fun ProfileScreen() {
+
+}
+
+@Preview
+@Composable
+private fun ProfileScreenPreview() {
+    ProfileScreen()
+}
---
+++ feature/profile/src/main/java/com/albrivas/fuelpump/profile/ProfileViewModel.kt
@@ -0,0 +1,6 @@
+package com.albrivas.fuelpump.profile
+
+import androidx.lifecycle.ViewModel
+
+class ProfileViewModel: ViewModel() {
+}
