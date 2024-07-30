ce905afc0f46a40e5d191c3dcd6e698b19efa22a
Alberto Rivas <albrivas95@outlook.com>
2024-07-30 13:07:34 +0200

    feat: show the fuel selected by the user

--- feature/profile/src/main/java/com/albrivas/fuelpump/profile/ProfileScreen.kt
+++ feature/profile/src/main/java/com/albrivas/fuelpump/profile/ProfileScreen.kt
@@ -1,43 +1,84 @@
 package com.albrivas.fuelpump.profile
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.albrivas.fuelpump.core.model.data.FuelType
+import com.albrivas.fuelpump.core.model.data.UserData
+import com.albrivas.fuelpump.core.ui.translation
 import com.albrivas.fuelpump.core.uikit.components.settings.SettingItem
 import com.albrivas.fuelpump.core.uikit.components.settings.SettingItemModel
 import com.albrivas.fuelpump.core.uikit.theme.MyApplicationTheme
 import com.albrivas.fuelpump.feature.profile.R
 
 @Composable
-internal fun ProfileScreenRoute() {
-    return ProfileScreen()
+internal fun ProfileScreenRoute(viewModel: ProfileViewModel = hiltViewModel()) {
+    val state by viewModel.userData.collectAsStateWithLifecycle()
+    return ProfileScreen(uiState = state)
 }
 
 @Composable
-internal fun ProfileScreen() {
-    Column(
-        modifier = Modifier
-            .background(color = Color.White)
-            .fillMaxSize()
-    ) {
-        SettingItem(
-            model = SettingItemModel(
-                title = stringResource(id = R.string.fuel_selection),
-                selection = "Gasolina 95"
-            )
-        )
+internal fun ProfileScreen(uiState: ProfileUiState) {
+    when (uiState) {
+        is ProfileUiState.Loading -> {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.White)
+                    .statusBarsPadding(),
+                contentAlignment = Alignment.Center
+            ) {
+                CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+            }
+        }
+
+        is ProfileUiState.Success -> {
+            Column(
+                modifier = Modifier
+                    .background(color = Color.White)
+                    .fillMaxSize()
+            ) {
+                SettingItem(
+                    model = SettingItemModel(
+                        title = stringResource(id = R.string.fuel_selection),
+                        selection = stringResource(id = uiState.userData.fuelSelection.translation()),
+                        onClick = {},
+                    )
+                )
+            }
+        }
+
+        is ProfileUiState.LoadFailed -> {
+            // Error state
+        }
     }
+
 }
 
 @Preview
 @Composable
 private fun ProfileScreenPreview() {
     MyApplicationTheme {
-        ProfileScreen()
+        ProfileScreen(uiState = ProfileUiState.Success(userData = UserData(fuelSelection = FuelType.GASOLINE_95)))
+    }
+}
+
+@Preview
+@Composable
+private fun ProfileScreenLoadingPreview() {
+    MyApplicationTheme {
+        ProfileScreen(uiState = ProfileUiState.Loading)
     }
 }
--- feature/profile/src/main/java/com/albrivas/fuelpump/profile/ProfileViewModel.kt
+++ feature/profile/src/main/java/com/albrivas/fuelpump/profile/ProfileViewModel.kt
@@ -1,5 +1,34 @@
 package com.albrivas.fuelpump.profile
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.albrivas.fuelpump.core.domain.GetUserDataUseCase
+import com.albrivas.fuelpump.core.domain.SaveUserDataUseCase
+import com.albrivas.fuelpump.core.model.data.FuelType
+import com.albrivas.fuelpump.core.model.data.UserData
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class ProfileViewModel : ViewModel()
+@HiltViewModel
+class ProfileViewModel @Inject constructor(
+    private val getUserDataUseCase: GetUserDataUseCase,
+    private val saveUserDataUseCase: SaveUserDataUseCase,
+) : ViewModel() {
+
+    val userData: StateFlow<ProfileUiState> = getUserDataUseCase().map {
+        ProfileUiState.Success(it)
+    }.stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(5_000),
+        initialValue = ProfileUiState.Loading
+    )
+
+    fun saveSelectionFuel(fuelType: FuelType) = viewModelScope.launch {
+        saveUserDataUseCase(UserData(fuelSelection = fuelType))
+    }
+}
