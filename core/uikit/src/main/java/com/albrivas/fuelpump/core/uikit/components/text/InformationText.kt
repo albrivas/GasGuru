9a3c164b7506bf41ec75d2b0b5afc8aaab25a632
Alberto Rivas <albrivas95@outlook.com>
2024-09-11 23:22:16 +0200

    chore: apply detekt autocorrections

--- core/uikit/src/main/java/com/albrivas/fuelpump/core/uikit/components/text/InformationText.kt
+++ core/uikit/src/main/java/com/albrivas/fuelpump/core/uikit/components/text/InformationText.kt
@@ -1,36 +1,27 @@
 package com.albrivas.fuelpump.core.uikit.components.text
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.MaterialTheme.typography
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.albrivas.fuelpump.core.uikit.R
 import com.albrivas.fuelpump.core.uikit.theme.GrayLight
 import com.albrivas.fuelpump.core.uikit.theme.MyApplicationTheme
-import com.albrivas.fuelpump.core.uikit.theme.primaryContainerLight
-import com.albrivas.fuelpump.core.uikit.theme.secondaryLight
 
 @Composable
 fun InformationText(modifier: Modifier = Modifier, model: InformationTextModel) = with(model) {
