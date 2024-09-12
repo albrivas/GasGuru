93e54fffa50c386ee08c945f0884c60c650be08a
Alberto Rivas <albrivas95@outlook.com>
2024-09-13 00:15:12 +0200

    fix: use app theme in test

--- feature/detail-station/src/androidTest/java/com/albrivas/fuelpump/feature/detail_station/ui/DetailStationScreenTest.kt
+++ feature/detail-station/src/androidTest/java/com/albrivas/fuelpump/feature/detail_station/ui/DetailStationScreenTest.kt
@@ -13,6 +13,7 @@ import androidx.compose.ui.test.performClick
 import com.albrivas.fuelpump.core.model.data.previewFuelStationDomain
 import com.albrivas.fuelpump.core.testing.BaseTest
 import com.albrivas.fuelpump.core.ui.IconTintKey
+import com.albrivas.fuelpump.core.uikit.theme.MyApplicationTheme
 import com.albrivas.fuelpump.core.uikit.theme.YellowFavorite
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
@@ -34,7 +35,9 @@ class DetailStationScreenTest : BaseTest() {
     @DisplayName("Show all the information about station")
     fun displayStationInformation() = extension.use {
         setContent {
-            DetailStationScreen(uiState = DetailStationUiState.Success(station = previewFuelStationDomain()))
+            MyApplicationTheme {
+                DetailStationScreen(uiState = DetailStationUiState.Success(station = previewFuelStationDomain()))
+            }
         }
 
         onNodeWithTag("address").assertIsDisplayed()
