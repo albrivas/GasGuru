302e0d49359f77e3d9ca10dd578290a7f306ba86
Alberto Rivas <albrivas95@outlook.com>
2024-10-06 19:02:20 +0200

    feat: apply new typography to welcome screen

--- core/uikit/src/main/java/com/albrivas/fuelpump/core/uikit/components/Button.kt
+++ core/uikit/src/main/java/com/albrivas/fuelpump/core/uikit/components/Button.kt
@@ -6,11 +6,12 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.albrivas.fuelpump.core.uikit.theme.FuelPumpTheme
 import com.albrivas.fuelpump.core.uikit.theme.GreenDark
 
 @Composable
@@ -24,7 +25,9 @@ fun FuelPumpButton(
     Button(
         onClick = onClick,
         shape = RoundedCornerShape(8.dp),
-        modifier = modifier.fillMaxWidth().height(50.dp),
+        modifier = modifier
+            .fillMaxWidth()
+            .height(50.dp),
         enabled = enabled,
         contentPadding = contentPadding,
         colors = ButtonDefaults.buttonColors(
@@ -33,7 +36,13 @@ fun FuelPumpButton(
     ) {
         Text(
             text = text,
-            style = MaterialTheme.typography.bodyMedium
+            style = FuelPumpTheme.typography.baseBold
         )
     }
+}
+
+@Preview
+@Composable
+private fun FuelPumpButtonPreview() {
+    FuelPumpButton(onClick = {}, text = "Siguiente")
 }
