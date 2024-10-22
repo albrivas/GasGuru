ca9f7f627acac9837e9d418149367638d830beec
Alberto Rivas <38986603+albrivas@users.noreply.github.com>
2024-10-22 17:16:46 +0200

    Feature - Redesign navigation bar (#122)
    * feat: add sheet to show a list with the nearby stations
    
    * feat: add container whit border to list station
    
    * feat: modify marker design and adjust sheet when is expanded
    
    * feat: add strings
    
    * feat: remove unnecessary padding
    
    * feat: remove unnecessary padding
    
    * chore: bump version from 1.1.1 to 1.1.2
    
    * feat: change icons navigation bar and add indicator color
    
    * chore: bump version from 1.1.2 to 1.1.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,4 @@
 import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
-import java.util.Properties
 
 plugins {
     alias(libs.plugins.android.application)
@@ -126,6 +125,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation(libs.kotlinx.serialization.json)
+    implementation(libs.material.icons.extended)
 
     // Firebase
     implementation(platform(libs.firebase.bom))
