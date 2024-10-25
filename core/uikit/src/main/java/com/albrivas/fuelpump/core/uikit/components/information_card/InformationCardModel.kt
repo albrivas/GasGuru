f3b939239a6387596e1c3b17fba679afa4edf48b
Alberto Rivas <38986603+albrivas@users.noreply.github.com>
2024-10-25 11:23:13 +0200

    Feature - Redesign detail station (#124)
    * feat: add new component to show the station schedule
    
    * chore: add new strings
    
    * feat: create new components and add the new design to detail screen
    
    * test: check the logic of favorite button
    
    * chore: bump version from 1.1.4 to 1.1.5
    
    * feat: unify information component in one component
    
    * feat: add information component to show station schedule

---
+++ core/uikit/src/main/java/com/albrivas/fuelpump/core/uikit/components/information_card/InformationCardModel.kt
@@ -0,0 +1,14 @@
+package com.albrivas.fuelpump.core.uikit.components.information_card
+
+data class InformationCardModel(
+    val title: String,
+    val subtitle: String,
+    val description: String? = null,
+    val icon: Int? = null,
+    val type: InformationCardType = InformationCardType.NONE,
+    val onClick: () -> Unit = {},
+) {
+    enum class InformationCardType {
+        NONE, EXPANDABLE
+    }
+}
