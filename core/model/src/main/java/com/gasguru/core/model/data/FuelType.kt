a3b93560353926bbe0b98d97947dc29be388bbf5
Alberto Rivas <38986603+albrivas@users.noreply.github.com>
2024-11-04 12:04:56 +0100

    Feature - More fuel types (#136)
    * feat: add new string and icons
    
    * feat: add bew fuel types
    
    * feat: add new fuel types
    
    * feat: rename diesel agricultural for gasoil b
    
    * chore: bump version from 1.1.9 to 1.1.10

--- core/model/src/main/java/com/gasguru/core/model/data/FuelType.kt
+++ core/model/src/main/java/com/gasguru/core/model/data/FuelType.kt
@@ -2,7 +2,11 @@ package com.gasguru.core.model.data
 
 enum class FuelType {
     GASOLINE_95,
+    GASOLINE_95_PREMIUM,
+    GASOLINE_95_E10,
     GASOLINE_98,
+    GASOLINE_98_PREMIUM,
     DIESEL,
     DIESEL_PLUS,
+    GASOIL_B,
 }
