7f38976120b516da3365bc79fe2a7eb79e5590d3
Alberto Rivas <albrivas95@outlook.com>
2024-11-19 15:20:55 +0100

    feat: add new logic to get fuel stations through filters

--- core/database/src/main/java/com/gasguru/core/database/dao/FuelStationDao.kt
+++ core/database/src/main/java/com/gasguru/core/database/dao/FuelStationDao.kt
@@ -10,18 +10,45 @@ import kotlinx.coroutines.flow.Flow
 @Suppress("Indentation")
 @Dao
 interface FuelStationDao {
+
+    fun getFuelStations(fuelType: String, brands: List<String>): Flow<List<FuelStationEntity>> {
+        return if (brands.isEmpty()) {
+            getFuelStationsWithoutBrandFilter(fuelType)
+        } else {
+            getFuelStationsWithBrandFilter(fuelType, brands)
+        }
+    }
+
     @Query(
         "SELECT * FROM `fuel-station` WHERE " +
+            "(" +
             "(:fuelType = 'GASOLINE_95' AND priceGasoline95E5 > 0) OR " +
             "(:fuelType = 'GASOLINE_95_PREMIUM' AND priceGasoline95E5Premium > 0) OR " +
             "(:fuelType = 'GASOLINE_95_E10' AND priceGasoline95E10 > 0) OR " +
-            "(:fuelType = 'GASOLINE_98' AND priceGasoline98E5 > 0 ) OR " +
+            "(:fuelType = 'GASOLINE_98' AND priceGasoline98E5 > 0) OR " +
             "(:fuelType = 'GASOLINE_98_PREMIUM' AND priceGasoline98E10 > 0) OR " +
-            "(:fuelType = 'DIESEL' AND priceGasoilA > 0 ) OR " +
+            "(:fuelType = 'DIESEL' AND priceGasoilA > 0) OR " +
             "(:fuelType = 'DIESEL_PLUS' AND priceGasoilPremium > 0) OR " +
-            "(:fuelType = 'GASOIL_B' AND priceGasoilB > 0)"
+            "(:fuelType = 'GASOIL_B' AND priceGasoilB > 0)" +
+            ")"
     )
-    fun getFuelStations(fuelType: String): Flow<List<FuelStationEntity>>
+    fun getFuelStationsWithoutBrandFilter(fuelType: String): Flow<List<FuelStationEntity>>
+
+    @Query(
+        "SELECT * FROM `fuel-station` WHERE " +
+            "(" +
+            "(:fuelType = 'GASOLINE_95' AND priceGasoline95E5 > 0) OR " +
+            "(:fuelType = 'GASOLINE_95_PREMIUM' AND priceGasoline95E5Premium > 0) OR " +
+            "(:fuelType = 'GASOLINE_95_E10' AND priceGasoline95E10 > 0) OR " +
+            "(:fuelType = 'GASOLINE_98' AND priceGasoline98E5 > 0) OR " +
+            "(:fuelType = 'GASOLINE_98_PREMIUM' AND priceGasoline98E10 > 0) OR " +
+            "(:fuelType = 'DIESEL' AND priceGasoilA > 0) OR " +
+            "(:fuelType = 'DIESEL_PLUS' AND priceGasoilPremium > 0) OR " +
+            "(:fuelType = 'GASOIL_B' AND priceGasoilB > 0)" +
+            ")" +
+            "AND brandStation IN (:brands) COLLATE NOCASE"
+    )
+    fun getFuelStationsWithBrandFilter(fuelType: String, brands: List<String>): Flow<List<FuelStationEntity>>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertFuelStation(items: List<FuelStationEntity>)
