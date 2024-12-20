9b1aff3ccba35abded266995d2c7247d9b0f14b8
Alberto Rivas <albrivas95@outlook.com>
2024-12-20 12:59:21 +0100

    feat: change how to call the ApiService

--- core/network/src/main/java/com/gasguru/core/network/datasource/RemoteDataSourceImp.kt
+++ core/network/src/main/java/com/gasguru/core/network/datasource/RemoteDataSourceImp.kt
@@ -2,13 +2,15 @@ package com.gasguru.core.network.datasource
 
 import arrow.core.Either
 import com.gasguru.core.network.common.tryCall
+import com.gasguru.core.network.di.NetworkModule
 import com.gasguru.core.network.model.NetworkError
 import com.gasguru.core.network.model.NetworkFuelStation
 import com.gasguru.core.network.retrofit.ApiService
 import javax.inject.Inject
+import javax.inject.Named
 
 class RemoteDataSourceImp @Inject constructor(
-    private val api: ApiService
+    @Named(NetworkModule.FUEL_API_SERVICE) private val api: ApiService
 ) : RemoteDataSource {
 
     override suspend fun getListFuelStations(): Either<NetworkError, NetworkFuelStation> = tryCall {
