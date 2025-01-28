cd4477d254667a229086156a1fe01d39c7ad67c3
Alberto Rivas <albrivas95@outlook.com>
2025-01-28 15:16:13 +0100

    feat: add product type

--- core/model/src/main/java/com/gasguru/core/model/data/FuelType.kt
+++ core/model/src/main/java/com/gasguru/core/model/data/FuelType.kt
@@ -1,12 +1,12 @@
 package com.gasguru.core.model.data
 
-enum class FuelType {
-    GASOLINE_95,
-    GASOLINE_95_PREMIUM,
-    GASOLINE_95_E10,
-    GASOLINE_98,
-    GASOLINE_98_PREMIUM,
-    DIESEL,
-    DIESEL_PLUS,
-    GASOIL_B,
+enum class FuelType(val idProduct: Int) {
+    GASOLINE_95(1),
+    GASOLINE_95_PREMIUM(20),
+    GASOLINE_95_E10(23),
+    GASOLINE_98(3),
+    GASOLINE_98_PREMIUM(21),
+    DIESEL(4),
+    DIESEL_PLUS(5),
+    GASOIL_B(6),
 }
