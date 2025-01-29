016ef81e2e1a033c0d999e143c20f371f0452434
Alberto Rivas <albrivas95@outlook.com>
2025-01-29 16:00:41 +0100

    feat: separate calls

--- feature/detail-station/src/main/java/com/gasguru/feature/detail_station/ui/DetailStationUiState.kt
+++ feature/detail-station/src/main/java/com/gasguru/feature/detail_station/ui/DetailStationUiState.kt
@@ -1,9 +1,16 @@
 package com.gasguru.feature.detail_station.ui
 
 import com.gasguru.core.model.data.FuelStation
+import com.gasguru.core.model.data.PriceHistory
 
 sealed interface DetailStationUiState {
     data object Loading : DetailStationUiState
     data class Success(val station: FuelStation, val address: String?) : DetailStationUiState
     data object Error : DetailStationUiState
+}
+
+sealed interface PriceHistoryUiState {
+    data class Success(val prices: List<PriceHistory>) : PriceHistoryUiState
+    data object Loading : PriceHistoryUiState
+    data object Error : PriceHistoryUiState
 }
