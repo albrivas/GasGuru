4ac1fe85b650475165b033f30c1e98ed3123ae5b
Alberto Rivas <albrivas95@outlook.com>
2025-03-17 12:51:23 +0100

    feat: add price and direction in station list

--- auto/common/src/main/java/com/gasguru/auto/di/CarScreenEntryPoint.kt
+++ auto/common/src/main/java/com/gasguru/auto/di/CarScreenEntryPoint.kt
@@ -1,5 +1,7 @@
 package com.gasguru.auto.di
 
+import com.gasguru.core.domain.FuelStationByLocationUseCase
+import com.gasguru.core.domain.GetUserDataUseCase
 import com.gasguru.core.domain.location.GetCurrentLocationUseCase
 import dagger.hilt.EntryPoint
 import dagger.hilt.InstallIn
@@ -9,4 +11,6 @@ import dagger.hilt.components.SingletonComponent
 @InstallIn(SingletonComponent::class)
 interface CarScreenEntryPoint {
     fun getCurrentLocationUseCase(): GetCurrentLocationUseCase
+    fun getFuelStationByLocation(): FuelStationByLocationUseCase
+    fun getUserDataUseCase(): GetUserDataUseCase
 }
--- auto/common/src/main/java/com/gasguru/auto/ui/FuelStationAutomotiveScreen.kt
+++ auto/common/src/main/java/com/gasguru/auto/ui/FuelStationAutomotiveScreen.kt
@@ -1,31 +1,66 @@
 package com.gasguru.auto.ui
 
 import android.Manifest
+import android.content.Intent
+import android.net.Uri
 import androidx.car.app.CarContext
 import androidx.car.app.Screen
-import androidx.car.app.model.Pane
-import androidx.car.app.model.PaneTemplate
+import androidx.car.app.model.Action
+import androidx.car.app.model.CarColor
+import androidx.car.app.model.CarLocation
+import androidx.car.app.model.ItemList
+import androidx.car.app.model.Metadata
+import androidx.car.app.model.Place
+import androidx.car.app.model.PlaceListMapTemplate
+import androidx.car.app.model.PlaceMarker
 import androidx.car.app.model.Row
 import androidx.car.app.model.Template
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
 import com.gasguru.auto.di.CarScreenEntryPoint
+import com.gasguru.core.domain.FuelStationByLocationUseCase
+import com.gasguru.core.domain.GetUserDataUseCase
 import com.gasguru.core.domain.location.GetCurrentLocationUseCase
+import com.gasguru.core.model.data.FuelStation
+import com.gasguru.core.model.data.FuelType
+import com.gasguru.core.model.data.OpeningHours
+import com.gasguru.core.ui.getPrice
+import com.gasguru.core.uikit.theme.Primary600
 import dagger.hilt.android.EntryPointAccessors
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.launch
 
 class FuelStationScreen(carContext: CarContext) : Screen(carContext) {
 
     private var hasLocationPermission = false
     private val coroutineScope = CoroutineScope(Dispatchers.Main)
-    private var currentLocationText: String = "Loading location..."
 
+    private var carUiState = CarUiState(
+        loading = true
+    )
 
     private val getCurrentLocationUseCase: GetCurrentLocationUseCase by lazy {
         EntryPointAccessors.fromApplication(
             carContext.applicationContext,
             CarScreenEntryPoint::class.java
         ).getCurrentLocationUseCase()
+    }
+
+    private val getFuelStationByLocation: FuelStationByLocationUseCase by lazy {
+        EntryPointAccessors.fromApplication(
+            carContext.applicationContext,
+            CarScreenEntryPoint::class.java
+        ).getFuelStationByLocation()
+    }
+
+    private val getUserDataUseCase: GetUserDataUseCase by lazy {
+        EntryPointAccessors.fromApplication(
+            carContext.applicationContext,
+            CarScreenEntryPoint::class.java
+        ).getUserDataUseCase()
     }
 
     init {
@@ -40,25 +75,109 @@ class FuelStationScreen(carContext: CarContext) : Screen(carContext) {
                 )
             ) {
                 hasLocationPermission = true
-                coroutineScope.launch {
-                    getCurrentLocationUseCase()?.let {
-                        val latitude = it.latitude
-                        val longitude = it.longitude
-                        currentLocationText = "Latitude: $latitude, Longitude: $longitude"
-                    }
-                    invalidate()
-                }
+                updateStationList()
             }
         }
     }
 
+    private fun updateStationList() {
+        coroutineScope.launch {
+            getCurrentLocationUseCase()?.let { location ->
+                combine(
+                    getUserDataUseCase(), getFuelStationByLocation(
+                        userLocation = location,
+                        maxStations = 25,
+                        brands = emptyList(),
+                        schedule = OpeningHours.NONE
+                    )
+                ) { userData, stations ->
+                    carUiState = CarUiState(
+                        loading = false,
+                        stations = stations,
+                        selectedFuel = userData.fuelSelection
+                    )
+                    invalidate()
+                }.launchIn(coroutineScope)
+            }
+        }
+    }
+
+    private fun cafeRow(station: FuelStation, selectedFuel: FuelType?): Row {
+        return Row.Builder()
+            .setTitle(
+                "${station.formatName()} - ${
+                    selectedFuel?.getPrice(
+                        carContext,
+                        station
+                    ) ?: ""
+                }"
+            )
+            .setMetadata(
+                Metadata.Builder()
+                    .setPlace(
+                        Place.Builder(
+                            CarLocation.create(
+                                station.location.latitude, station.location.longitude
+                            )
+                        ).setMarker(
+                            PlaceMarker.Builder().setColor(
+                                CarColor.createCustom(
+                                    Color.White.toArgb(), Primary600.toArgb(),
+                                )
+                            ).build()
+                        )
+                            .build()
+                    ).build()
+            )
+            .setBrowsable(true)
+            .setOnClickListener {
+                navigateToStation(station.location.latitude, station.location.longitude)
+            }
+            .addText(station.formatDirection())
+            .addText(station.formatDistance())
+            .build()
+    }
+
+    private fun navigateToStation(latitude: Double, longitude: Double) {
+        val intent = Intent().apply {
+            action = CarContext.ACTION_NAVIGATE
+            data = Uri.parse("geo:$latitude,$longitude")
+        }
+        carContext.startCarApp(intent)
+        screenManager.pop()
+    }
+
     override fun onGetTemplate(): Template {
-        return PaneTemplate.Builder(
-            Pane.Builder()
-                .addRow(
-                    Row.Builder().setTitle(currentLocationText).build()
-                )
-                .build()
-        ).build()
+        val builder = PlaceListMapTemplate
+            .Builder()
+            .setTitle("GasGuru Finder")
+            .setHeaderAction(Action.APP_ICON)
+
+        builder.setLoading(carUiState.loading)
+
+        if (!carUiState.loading) {
+            builder.setLoading(false)
+            val items = ItemList.Builder()
+
+            carUiState.stations.forEach { station ->
+                items.addItem(cafeRow(station, carUiState.selectedFuel))
+            }
+
+            builder.setItemList(items.build())
+        }
+
+        if (hasLocationPermission) {
+            builder.setCurrentLocationEnabled(true)
+        }
+
+        builder.setOnContentRefreshListener { updateStationList() }
+
+        return builder.build()
     }
 }
+
+data class CarUiState(
+    val loading: Boolean = false,
+    val stations: List<FuelStation> = emptyList(),
+    val selectedFuel: FuelType? = null,
+)
