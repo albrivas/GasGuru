0ba1a855e4c21c8ccb7fecb39e35bf7399448feb
Alberto Rivas <38986603+albrivas@users.noreply.github.com>
2025-03-17 15:25:05 +0100

    Feature - Automotive (#236)
    * feat: implement base of android auto
    
    * feat: create new module auto and common to separate the implementation of android auto
    
    * feat: use convention plugins in module auto
    
    * chore: rename classes
    
    * feat: add a service required for the application support android auto compatibility
    
    * feat: remove unnecessary setup for android auto in module app
    
    * fix: remove permission because it causes the app not to compile
    
    * feat: create a entrypoint to use usecase in car screen
    
    * build: add necessary module dependencies
    
    * refactor: create a function to format brand station name. Refactor the function to calculate bounds in map
    
    * feat: add price and direction in station list
    
    * refactor: separate state ui in a file
    
    * chore: apply detekt warnings
    
    * fix: establish allowBackup as false
    
    * refactor: order fields

--- core/model/src/main/java/com/gasguru/core/model/data/OpeningHours.kt
+++ core/model/src/main/java/com/gasguru/core/model/data/OpeningHours.kt
@@ -4,5 +4,10 @@ package com.gasguru.core.model.data
  * Domain enum. Represents the opening hours of the fuel stations
  */
 enum class OpeningHours {
-    NONE, OPEN_NOW, OPEN_24H
+    /**
+     * Default option. Filter both cases OPEN_NOW and OPEN_24H
+     */
+    NONE,
+    OPEN_NOW,
+    OPEN_24H
 }
