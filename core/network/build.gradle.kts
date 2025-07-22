8214faceccc6bb28d04ef96531e648e0e1ea2fa2
Alberto Rivas <38986603+albrivas@users.noreply.github.com>
2025-07-22 10:50:50 +0200

    Feature - Domain test (#275)
    * test: create test for GetAddressFromLocationUseCase
    
    * fix: remove unnecessary implementations because these are in the test module

--- core/network/build.gradle.kts
+++ core/network/build.gradle.kts
@@ -30,8 +30,6 @@ dependencies {
     implementation(libs.kotlin.coroutines.play)
     implementation(libs.places)
 
-    testImplementation(libs.kotlinx.coroutines.test)
     testImplementation(libs.mock.webserver)
     testImplementation(projects.core.testing)
-    testImplementation(libs.mockk)
 }
