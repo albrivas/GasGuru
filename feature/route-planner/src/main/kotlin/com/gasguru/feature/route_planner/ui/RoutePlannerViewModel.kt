001811b2d843ab48b173fa93423ac1a4df5a95cf
Alberto Rivas <albrivas95@outlook.com>
2025-08-01 17:24:00 +0200

    feat: remove unused state for the moment

--- feature/route-planner/src/main/kotlin/com/gasguru/feature/route_planner/ui/RoutePlannerViewModel.kt
+++ feature/route-planner/src/main/kotlin/com/gasguru/feature/route_planner/ui/RoutePlannerViewModel.kt
@@ -2,13 +2,10 @@ package com.gasguru.feature.route_planner.ui
 
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
 
 @HiltViewModel
 class RoutePlannerViewModel : ViewModel() {
 
-    private val _state = MutableStateFlow(RoutePlannerUiState.Loading)
-    val state = _state.asStateFlow()
+
 }
 
