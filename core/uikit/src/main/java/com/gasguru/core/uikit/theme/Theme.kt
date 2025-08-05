dc15e7bb0e0d9cb32194d61544c01a72781df209
Alberto Rivas <albrivas95@outlook.com>
2025-08-05 18:25:46 +0200

    feat: replace deprecated statusBarColor

--- core/uikit/src/main/java/com/gasguru/core/uikit/theme/Theme.kt
+++ core/uikit/src/main/java/com/gasguru/core/uikit/theme/Theme.kt
@@ -7,16 +7,12 @@ import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 @Composable
 fun MyApplicationTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
     typography: GasGuruTypography = GasGuruTheme.typography,
     content: @Composable () -> Unit,
 ) {
@@ -25,9 +21,10 @@ fun MyApplicationTheme(
     val view = LocalView.current
     if (!view.isInEditMode) {
         SideEffect {
-            val window = (view.context as Activity).window
-            window.statusBarColor = Color.Transparent.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
+            val activity = view.context as Activity
+            val insetsController = WindowCompat.getInsetsController(activity.window, view)
+            insetsController.isAppearanceLightStatusBars = !darkTheme
+            insetsController.isAppearanceLightNavigationBars = !darkTheme
         }
     }
 
