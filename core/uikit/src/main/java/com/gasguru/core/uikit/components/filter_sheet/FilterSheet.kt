1feb7a7824a41bf03ceae95b907fd10c2719218f
Alberto Rivas <albrivas95@outlook.com>
2025-08-05 18:50:46 +0200

    feat: use statusBarsPadding to expand sheet until the system bar

--- core/uikit/src/main/java/com/gasguru/core/uikit/components/filter_sheet/FilterSheet.kt
+++ core/uikit/src/main/java/com/gasguru/core/uikit/components/filter_sheet/FilterSheet.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
@@ -69,7 +70,7 @@ fun FilterSheet(model: FilterSheetModel, modifier: Modifier = Modifier) {
                 )
             }
         },
-        modifier = modifier
+        modifier = modifier.statusBarsPadding()
     ) {
         FilterSheetContent(model = model, onDismiss = {
             coroutineScope.launch { state.hide() }.invokeOnCompletion {
