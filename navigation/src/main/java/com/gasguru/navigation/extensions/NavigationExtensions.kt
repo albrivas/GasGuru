1993ceb7259d57a79c4e00cc7d87f4c59798f734
Alberto Rivas <albrivas95@outlook.com>
2025-08-20 18:13:56 +0200

    refactor: change name

--- navigation/src/main/java/com/gasguru/navigation/extensions/NavigationExtensions.kt
+++ navigation/src/main/java/com/gasguru/navigation/extensions/NavigationExtensions.kt
@@ -3,9 +3,7 @@ package com.gasguru.navigation.extensions
 import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavController
 
-/**
- * Generic extension function to set any value in the previous back stack entry's savedStateHandle
- */
+
 inline fun <reified T> NavController.setPreviousResult(key: String, value: T?) {
     previousBackStackEntry?.savedStateHandle?.set(key, value)
 }
