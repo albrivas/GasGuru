e0ce24c07dc0b83c1df021c719942e117e1a8a59
Alberto Rivas <albrivas95@outlook.com>
2025-08-28 21:32:28 +0200

    feat: use new dao to get favorite stations

--- core/database/src/main/java/com/gasguru/core/database/dao/FavoriteStationDao.kt
+++ core/database/src/main/java/com/gasguru/core/database/dao/FavoriteStationDao.kt
@@ -8,23 +8,25 @@ import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface FavoriteStationDao {
-    
+
     @Query("SELECT idServiceStation FROM favorite_stations")
     fun getFavoriteStationIds(): Flow<List<Int>>
-    
+
     @Query("INSERT OR IGNORE INTO favorite_stations (idServiceStation) VALUES (:stationId)")
     suspend fun addFavoriteStation(stationId: Int)
-    
+
     @Query("DELETE FROM favorite_stations WHERE idServiceStation = :stationId")
     suspend fun removeFavoriteStation(stationId: Int)
-    
+
     @Query("SELECT EXISTS(SELECT 1 FROM favorite_stations WHERE idServiceStation = :stationId)")
     suspend fun isFavorite(stationId: Int): Boolean
-    
+
     @Transaction
-    @Query("""
+    @Query(
+        """
         SELECT fs.* FROM `fuel-station` fs 
         INNER JOIN favorite_stations fav ON fs.idServiceStation = fav.idServiceStation
-    """)
+    """
+    )
     fun getFavoriteStations(): Flow<List<FuelStationEntity>>
 }
