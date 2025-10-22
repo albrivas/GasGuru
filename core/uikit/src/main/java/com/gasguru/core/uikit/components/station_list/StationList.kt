84de718e6e21db832f2066c2be049059253c3d70
Alberto Rivas <38986603+albrivas@users.noreply.github.com>
2025-10-22 16:48:56 +0200

    Bugfix - Visibility text "Show list" (#389)
    * fix: show text if the bottom sheet state is partially expanded
    
    * fix: show text if the bottom sheet state is partially expanded
    
    * fix: order list with an animation and then go to first item of the list

--- core/uikit/src/main/java/com/gasguru/core/uikit/components/station_list/StationList.kt
+++ core/uikit/src/main/java/com/gasguru/core/uikit/components/station_list/StationList.kt
@@ -9,6 +9,8 @@ import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.testTag
@@ -18,6 +20,7 @@ import com.gasguru.core.uikit.components.fuelItem.FuelStationItemModel
 import com.gasguru.core.uikit.components.swipe.SwipeItem
 import com.gasguru.core.uikit.components.swipe.SwipeItemModel
 import com.gasguru.core.uikit.theme.GasGuruTheme
+import kotlinx.coroutines.flow.distinctUntilChanged
 
 @Composable
 fun StationList(
@@ -28,6 +31,17 @@ fun StationList(
     testTag: String = "station_list",
     listState: LazyListState = rememberLazyListState()
 ) {
+    // Wait until animatedItem finished, to go to the first item of the list
+    LaunchedEffect(stations) {
+        snapshotFlow { listState.layoutInfo.totalItemsCount }
+            .distinctUntilChanged()
+            .collect {
+                if (listState.firstVisibleItemIndex != 0) {
+                    listState.animateScrollToItem(0)
+                }
+            }
+    }
+
     LazyColumn(
         state = listState,
         modifier = modifier
