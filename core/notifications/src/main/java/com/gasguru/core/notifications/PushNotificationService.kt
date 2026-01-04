c4542137de9d0b34e8ef5752198e50618c8f531f
Alberto Rivas <albrivas95@outlook.com>
2026-01-04 20:30:50 +0100

    chore: apply detekt warnings

--- core/notifications/src/main/java/com/gasguru/core/notifications/PushNotificationService.kt
+++ core/notifications/src/main/java/com/gasguru/core/notifications/PushNotificationService.kt
@@ -40,8 +40,8 @@ class PushNotificationService @Inject constructor(
                 addCategory(Intent.CATEGORY_LAUNCHER)
                 putExtra(KEY_STATION_ID, stationId)
                 flags = Intent.FLAG_ACTIVITY_NEW_TASK or
-                        Intent.FLAG_ACTIVITY_SINGLE_TOP or
-                        Intent.FLAG_ACTIVITY_CLEAR_TOP
+                    Intent.FLAG_ACTIVITY_SINGLE_TOP or
+                    Intent.FLAG_ACTIVITY_CLEAR_TOP
             }
             context.startActivity(intent)
         }
