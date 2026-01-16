c7f94d0aa1bba7853d275205d56778b46abb03f9
Alberto Rivas <albrivas95@outlook.com>
2026-01-16 10:50:18 +0100

    refactor: add explicit type in all use case for a more readability

--- core/domain/src/main/java/com/gasguru/core/domain/fuelstation/GetFavoriteStationsUseCase.kt
+++ core/domain/src/main/java/com/gasguru/core/domain/fuelstation/GetFavoriteStationsUseCase.kt
@@ -1,12 +1,14 @@
 package com.gasguru.core.domain.fuelstation
 
-import com.gasguru.core.model.data.LatLng
 import com.gasguru.core.data.repository.user.UserDataRepository
+import com.gasguru.core.model.data.LatLng
+import com.gasguru.core.model.data.UserWithFavoriteStations
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class GetFavoriteStationsUseCase @Inject constructor(
     private val repository: UserDataRepository,
 ) {
-    operator fun invoke(userLocation: LatLng) =
+    operator fun invoke(userLocation: LatLng): Flow<UserWithFavoriteStations> =
         repository.getUserWithFavoriteStations(userLocation = userLocation)
 }
--- core/domain/src/main/java/com/gasguru/core/domain/fuelstation/GetFuelStationByIdUseCase.kt
+++ core/domain/src/main/java/com/gasguru/core/domain/fuelstation/GetFuelStationByIdUseCase.kt
@@ -1,12 +1,14 @@
 package com.gasguru.core.domain.fuelstation
 
-import com.gasguru.core.model.data.LatLng
 import com.gasguru.core.data.repository.stations.OfflineFuelStationRepository
+import com.gasguru.core.model.data.FuelStation
+import com.gasguru.core.model.data.LatLng
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class GetFuelStationByIdUseCase @Inject constructor(
     private val repository: OfflineFuelStationRepository,
 ) {
-    operator fun invoke(id: Int, userLocation: LatLng) =
+    operator fun invoke(id: Int, userLocation: LatLng): Flow<FuelStation> =
         repository.getFuelStationById(id, userLocation)
 }
