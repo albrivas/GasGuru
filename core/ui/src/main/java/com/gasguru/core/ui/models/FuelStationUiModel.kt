c4de65c4a5c4b1a1c808f76251f9dc6834a45668
Alberto Rivas <albrivas95@outlook.com>
2026-01-29 17:19:56 +0100

    perf: add stable annotation to fuel station ui model

--- core/ui/src/main/java/com/gasguru/core/ui/models/FuelStationUiModel.kt
+++ core/ui/src/main/java/com/gasguru/core/ui/models/FuelStationUiModel.kt
@@ -1,8 +1,10 @@
 package com.gasguru.core.ui.models
 
 import androidx.annotation.DrawableRes
+import androidx.compose.runtime.Stable
 import com.gasguru.core.model.data.FuelStation
 
+@Stable
 data class FuelStationUiModel(
     val fuelStation: FuelStation,
     @DrawableRes val brandIcon: Int,
