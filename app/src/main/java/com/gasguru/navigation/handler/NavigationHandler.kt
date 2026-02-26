88fab65e25d78732a23b2492fcd8708fac009b3a
Alberto Rivas <albrivas95@outlook.com>
2026-02-26 16:28:43 +0100

    style: fix import order and blank lines

--- app/src/main/java/com/gasguru/navigation/handler/NavigationHandler.kt
+++ app/src/main/java/com/gasguru/navigation/handler/NavigationHandler.kt
@@ -4,8 +4,8 @@ import androidx.navigation.NavController
 import androidx.navigation.NavOptions
 import com.gasguru.feature.detail_station.navigation.navigateToDetailStation
 import com.gasguru.feature.detail_station.navigation.navigateToDetailStationAsDialog
-import com.gasguru.feature.onboarding_welcome.navigation.navigateToCapacityTankRoute
 import com.gasguru.feature.onboarding_welcome.navigation.OnboardingRoutes
+import com.gasguru.feature.onboarding_welcome.navigation.navigateToCapacityTankRoute
 import com.gasguru.feature.onboarding_welcome.navigation.navigateToNewOnboardingRoute
 import com.gasguru.feature.onboarding_welcome.navigation.navigateToOnboardingFuelPreferencesRoute
 import com.gasguru.feature.route_planner.navigation.navigateToRoutePlannerScreen
