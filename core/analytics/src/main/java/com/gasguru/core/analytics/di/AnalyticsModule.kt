2393c401035a7da11c942fb032011a133afb6770
Alberto Rivas <albrivas95@outlook.com>
2026-03-18 18:58:16 +0100

    refactor(analytics): move MixpanelAPI binding to app module

--- core/analytics/src/main/java/com/gasguru/core/analytics/di/AnalyticsModule.kt
+++ core/analytics/src/main/java/com/gasguru/core/analytics/di/AnalyticsModule.kt
@@ -4,14 +4,9 @@ import com.gasguru.core.analytics.AnalyticsHelper
 import com.gasguru.core.analytics.LogcatAnalyticsHelper
 import com.gasguru.core.analytics.MixpanelAnalyticsHelper
 import com.mixpanel.android.BuildConfig
-import com.mixpanel.android.mpmetrics.MixpanelAPI
-import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
 val analyticsModule = module {
-    single<MixpanelAPI> {
-        MixpanelAPI.getInstance(androidContext(), null, true)
-    }
     single<AnalyticsHelper> {
         if (BuildConfig.DEBUG) {
             LogcatAnalyticsHelper()
