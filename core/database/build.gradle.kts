de4983bf39942f4766272f511f2b2a7727d310fb
Alberto Rivas <albrivas95@outlook.com>
2026-04-07 19:07:46 +0200

    build: add detekt support to kmp library convention plugin

--- core/database/build.gradle.kts
+++ core/database/build.gradle.kts
@@ -25,10 +25,12 @@ kotlin {
         }
         androidUnitTest.dependencies {
             implementation(libs.mockk)
+            implementation(libs.junit5.api)
             runtimeOnly(libs.junit5.engine)
             implementation(libs.kotlinx.coroutines.test)
             implementation(libs.turbine)
             implementation(libs.androidx.sqlite.bundled)
+            implementation(libs.androidx.room.runtime)
         }
     }
 }
