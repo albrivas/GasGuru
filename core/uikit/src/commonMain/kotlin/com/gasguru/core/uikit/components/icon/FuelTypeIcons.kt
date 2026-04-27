1b7ff8f35d7499a0bafaabe1afbfda1491a257a9
Alberto Rivas <albrivas95@outlook.com>
2026-04-27 17:57:31 +0200

    feat: add public icon wrappers in core:uikit

---
+++ core/uikit/src/commonMain/kotlin/com/gasguru/core/uikit/components/icon/FuelTypeIcons.kt
@@ -0,0 +1,25 @@
+package com.gasguru.core.uikit.components.icon
+
+import com.gasguru.core.uikit.generated.resources.Res
+import com.gasguru.core.uikit.generated.resources.ic_adblue
+import com.gasguru.core.uikit.generated.resources.ic_diesel
+import com.gasguru.core.uikit.generated.resources.ic_diesel_plus
+import com.gasguru.core.uikit.generated.resources.ic_gasoline_95
+import com.gasguru.core.uikit.generated.resources.ic_gasoline_95_e10
+import com.gasguru.core.uikit.generated.resources.ic_gasoline_95_premium
+import com.gasguru.core.uikit.generated.resources.ic_gasoline_98
+import com.gasguru.core.uikit.generated.resources.ic_gasoline_98_premium
+import com.gasguru.core.uikit.generated.resources.ic_gasoleo_b
+import org.jetbrains.compose.resources.DrawableResource
+
+object FuelTypeIcons {
+    val Gasoline95: DrawableResource = Res.drawable.ic_gasoline_95
+    val Gasoline98: DrawableResource = Res.drawable.ic_gasoline_98
+    val Diesel: DrawableResource = Res.drawable.ic_diesel
+    val DieselPlus: DrawableResource = Res.drawable.ic_diesel_plus
+    val Gasoline95Premium: DrawableResource = Res.drawable.ic_gasoline_95_premium
+    val Gasoline95E10: DrawableResource = Res.drawable.ic_gasoline_95_e10
+    val Gasoline98Premium: DrawableResource = Res.drawable.ic_gasoline_98_premium
+    val GasoilB: DrawableResource = Res.drawable.ic_gasoleo_b
+    val Adblue: DrawableResource = Res.drawable.ic_adblue
+}
