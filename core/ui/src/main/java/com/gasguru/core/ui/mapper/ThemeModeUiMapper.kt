4f1a831ac351f92a8a87ae8ce18a0d653654cf75
Alberto Rivas <albrivas95@outlook.com>
2026-04-27 17:59:24 +0200

    refactor: update core:ui models and mappers for CMP types

--- core/ui/src/main/java/com/gasguru/core/ui/mapper/ThemeModeUiMapper.kt
+++ core/ui/src/main/java/com/gasguru/core/ui/mapper/ThemeModeUiMapper.kt
@@ -3,7 +3,7 @@ package com.gasguru.core.ui.mapper
 import com.gasguru.core.model.data.ThemeMode
 import com.gasguru.core.ui.R
 import com.gasguru.core.ui.models.ThemeModeUi
-import com.gasguru.core.uikit.R as RUikit
+import com.gasguru.core.uikit.components.icon.ThemeModeIcons
 
 /**
  * Maps [ThemeMode] domain model to [ThemeModeUi].
@@ -12,7 +12,7 @@ import com.gasguru.core.uikit.R as RUikit
  * @return ThemeModeUi UI representation with icon and string resources
  */
 fun ThemeMode.toUi(): ThemeModeUi = when (this) {
-    ThemeMode.DARK -> ThemeModeUi(this, R.string.theme_mode_dark, RUikit.drawable.ic_dark_mode)
-    ThemeMode.LIGHT -> ThemeModeUi(this, R.string.theme_mode_light, RUikit.drawable.ic_light_mode)
-    ThemeMode.SYSTEM -> ThemeModeUi(this, R.string.theme_mode_system, RUikit.drawable.ic_system_mode)
+    ThemeMode.DARK -> ThemeModeUi(this, R.string.theme_mode_dark, ThemeModeIcons.Dark)
+    ThemeMode.LIGHT -> ThemeModeUi(this, R.string.theme_mode_light, ThemeModeIcons.Light)
+    ThemeMode.SYSTEM -> ThemeModeUi(this, R.string.theme_mode_system, ThemeModeIcons.System)
 }
