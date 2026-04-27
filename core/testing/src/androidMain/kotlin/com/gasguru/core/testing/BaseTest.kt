8c9d09b0c00b83e74626e96fa29d0e573755c097
Alberto Rivas <albrivas95@outlook.com>
2026-04-27 22:45:09 +0200

    test: add getCmpString helper to BaseTest

--- core/testing/src/androidMain/kotlin/com/gasguru/core/testing/BaseTest.kt
+++ core/testing/src/androidMain/kotlin/com/gasguru/core/testing/BaseTest.kt
@@ -5,6 +5,9 @@ import androidx.annotation.StringRes
 import androidx.compose.ui.test.ExperimentalTestApi
 import androidx.test.core.app.ApplicationProvider
 import de.mannodermaus.junit5.compose.createComposeExtension
+import kotlinx.coroutines.runBlocking
+import org.jetbrains.compose.resources.StringResource
+import org.jetbrains.compose.resources.getString as cmpGetString
 import org.junit.jupiter.api.extension.RegisterExtension
 
 open class BaseTest {
@@ -19,4 +22,6 @@ open class BaseTest {
 
     fun getStringResource(@StringRes id: Int, vararg formatArgs: Any) =
         testContext.getString(id, *formatArgs)
+
+    fun getCmpString(resource: StringResource): String = runBlocking { cmpGetString(resource) }
 }
