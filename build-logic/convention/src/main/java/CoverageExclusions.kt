ef175fe7fe41b5e1112a954bff8718f7d1fe3eca
Alberto Rivas <albrivas95@outlook.com>
2026-05-04 13:44:49 +0200

    build: exclude composeResources generated files from jacoco

--- build-logic/convention/src/main/java/CoverageExclusions.kt
+++ build-logic/convention/src/main/java/CoverageExclusions.kt
@@ -42,6 +42,7 @@ object CoverageExclusions {
         "**/*App.*",
         "**/*Preferences.*",
         "**/res/**",
+        "**/generated/resources/**",
         "**/*ConnectivityManagerNetworkMonitor*",
         "**/*LocationTrackerRepository*",
         "**/*GeocoderAddressImpl*",
